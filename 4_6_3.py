01770ae0b53cdabc987c83e5149bbbc4913cdee4
eviscardi <ev2334@barnard.edu>
2015-03-13 00:44:47 +0000

    finished

--- 4_6_3.py
+++ 4_6_3.py
@@ -1,5 +1,12 @@
-# The following code makes a list of 10 random numbers between 0 and 10 from random import random as rand
+# The following code makes a list of 10 random numbers between 0 and 10\
+from random import random as rand
 x = []
 for i in range(10):
      x.append( rand() * 10 )
 print x
+
+greatest = 0
+for z in x:
+     if z > greatest:
+          greatest = z
+print greatest
